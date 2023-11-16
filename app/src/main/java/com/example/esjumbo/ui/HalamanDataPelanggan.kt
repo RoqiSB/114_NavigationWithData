c4954b06a1315ab7e9c55e11a9d23025432d4727
roqisb <roqisb9@gmail.com>
2023-11-16 16:56:39 +0700

    add OutlinedTextField

--- app/src/main/java/com/example/esjumbo/ui/HalamanDataPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/ui/HalamanDataPelanggan.kt
@@ -1,11 +1,22 @@
 package com.example.esjumbo.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.esjumbo.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanDataPelanggan(){
     var namaTxt by remember {
@@ -18,4 +29,34 @@ fun HalamanDataPelanggan(){
         mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(namaTxt,alamatTxt,tlpnTxt)
+
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        OutlinedTextField(
+            value = namaTxt,
+            onValueChange = {
+                namaTxt = it
+            },
+            label = { Text(text = stringResource(id = R.string.nama)) }
+        )
+
+        OutlinedTextField(
+            value = alamatTxt,
+            onValueChange = {
+                alamatTxt = it
+            },
+            label = { Text(text = stringResource(id = R.string.alamat)) }
+        )
+
+        OutlinedTextField(
+            value = tlpnTxt,
+            onValueChange = {
+                tlpnTxt = it
+            },
+            label = { Text(text = stringResource(id = R.string.telepon)) }
+        )
+    }
 }
