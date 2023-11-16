359d0edcffa82cca05c0be150054f0644bb3571d
roqisb <roqisb9@gmail.com>
2023-11-16 16:58:13 +0700

    add button onClick in HalamanDataPelanggan

--- app/src/main/java/com/example/esjumbo/ui/HalamanDataPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/ui/HalamanDataPelanggan.kt
@@ -2,7 +2,10 @@ package com.example.esjumbo.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -14,11 +17,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.esjumbo.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HalamanDataPelanggan(){
+fun HalamanDataPelanggan(
+    onSubmitButtonClicked: (MutableList<String>) -> Unit){
     var namaTxt by remember {
         mutableStateOf("")
     }
@@ -58,5 +63,10 @@ fun HalamanDataPelanggan(){
             },
             label = { Text(text = stringResource(id = R.string.telepon)) }
         )
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Button(onClick = { onSubmitButtonClicked(listData) }) {
+            Text(text = stringResource(id = R.string.btn_submit))
+        }
     }
 }
