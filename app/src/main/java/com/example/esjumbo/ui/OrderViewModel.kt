8e875768bb0587705febcc3eb437e8ad149a7426
roqisb <roqisb9@gmail.com>
2023-11-16 17:21:21 +0700

    add viewmodel for datapelanggan

--- app/src/main/java/com/example/esjumbo/ui/OrderViewModel.kt
+++ app/src/main/java/com/example/esjumbo/ui/OrderViewModel.kt
@@ -2,6 +2,7 @@ package com.example.esjumbo.ui
 
 import androidx.lifecycle.ViewModel
 import com.example.esjumbo.data.OrderUiState
+import com.example.esjumbo.data.PelangganUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,8 +13,17 @@ import java.text.NumberFormat
 private const val HARGA_PER_CUP = 3000
 class OrderViewModel: ViewModel() {
     private val _stateUI = MutableStateFlow(OrderUiState())
+    private val _pelangganUI = MutableStateFlow(PelangganUiState())
     val stateUI: StateFlow<OrderUiState> = _stateUI.asStateFlow()
+    val pelangganUI : StateFlow<PelangganUiState> = _pelangganUI.asStateFlow()
 
+    fun setData(list: MutableList<String>){
+        _pelangganUI.update { stateSaatIni -> stateSaatIni.copy(
+            nama = list[0],
+            alamat = list[1],
+            tlp = list[2]
+        ) }
+    }
     fun setJumlah(jmlEsJumbo:Int){
         _stateUI.update { stateSaatini ->
             stateSaatini.copy(
